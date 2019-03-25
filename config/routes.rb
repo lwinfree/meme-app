e22f4ff319153d99653a7c21468f91890392fd64
lilly <lillywinfree@gmail.com>
2019-03-25 14:11:29 -0700

    update frontend files

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,9 @@ Rails.application.routes.draw do
     delete "/favorites/:id" => "favorites#destroy"
   end
 
+
+
+  get "/*path" => proc { [200, {}, [ActionView::Base.new.render(file: 'public/index.html')]] } 
+
+
 end
