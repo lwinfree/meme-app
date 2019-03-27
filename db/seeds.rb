74d64144847ea2d45aa0763ea021ea625750fd39
lilly <lillywinfree@gmail.com>
2019-03-27 13:35:19 -0700

    adds updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
 User.create!([
-  {name: "Katrina", email: "katrina@gmail.com", zipcode: 79701, password_digest: "$2a$10$ixiySh.85e6YTm1gzSTARuYRIiQgrK6N/j9O2BPuEzfi4dMGyEHga"},
-  {name: "Lilly", email: "lilly@gmail.com", zipcode: 97214, password_digest: "$2a$10$zoIT1edRsXnxFYvqzhgfnuYXf2RE24CPJ8fa5..K7DVf00SJhLRbS"},
   {name: "Robin", email: "robin@gmail.com", zipcode: 77441, password_digest: "$2a$10$Mr74s4DaHv7YTnEd5aU7de.yuhbnrD0IRUWes9xWJcYKTvcvp4GGC"},
   {name: "Diane", email: "diane@gmail.com", zipcode: 77092, password_digest: "$2a$10$f8rsS2PNDhMAoB6mXZGTeOwa5vtBPlSt3w81Wom9t0lK4c54tF3R2"},
 ])
